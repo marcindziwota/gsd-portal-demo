79c6e3007ee575599a9f2d3a083c407a8ce899cc
Marcin Dziwota <marcin.dziwota@gmail.com>
2017-01-23 20:21:31 +0100

    error: syntax error at or near "' + myConfig.schema + '"

--- server/routes/request.ts
+++ server/routes/request.ts
@@ -12,7 +12,7 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dr
   var client = new pg.Client(connectionString);
   client.connect();
  
-    client.query(`INSERT INTO ' + myConfig.schema + 'GSD_Request__c (HealthcareEntity__c, RequestTitle__c, PublicDescription__c, TechnicalStatus__c) VALUES ($1, $2, $3, $4);`, 
+    client.query('INSERT INTO ' + myConfig.schema + 'GSD_Request__c (HealthcareEntity__c, RequestTitle__c, PublicDescription__c, TechnicalStatus__c) VALUES ($1, $2, $3, $4);', 
                     [request.body.sfid, request.body.title, request.body.description, 'Draft'], function(error, data) {
       if (error !== null) {
           console.log('error');
